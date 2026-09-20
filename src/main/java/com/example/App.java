6e04ec3490041a2da0d63858e2187cff4c957a07
Tanuja Mehra <2022wa86279@wilp.bits-pilani.ac.in>
2026-09-20 12:15:17 +0530

    Modified application message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello this is Tanuja Mehra from BITS DevOps Lab");
+        System.out.println("Hello this is Tanuja Mehra from BITS DevOps Lab - Continous Integration");
 
     }
 

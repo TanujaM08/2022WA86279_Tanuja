cca3049d9dd7229fd3a8d073003fdc66af7c4830
Tanuja Mehra <2022wa86279@wilp.bits-pilani.ac.in>
2026-09-20 10:36:10 +0530

    Initial Maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello this is Tanuja Mehra from BITS DevOps Lab");
+
+    }
+
+}
